44852b5049bd8da02324ce29e46aafedab704446
Lisa Kohls <lk210@hdm-stuttgart.de>
2024-11-01 17:41:59 +0000

    Removed client submodule and added it to the main project

---
+++ client/tailwind.config.js
@@ -0,0 +1,16 @@
+﻿/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js,tsx}"],
+    theme: {
+        colors: {
+            'primary': '#163b49',
+            'white': '#ffffff',
+        },
+        extend: {
+            fontFamily:{
+                'redhat' : ['Red Hat Display', 'sans-serif']
+            },
+        },
+    },
+    plugins: [],
+}
