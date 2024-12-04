fa22e9dc2fc1610ccd0d1539018dc7e18dbbd243
Lisa Kohls <lk210@hdm-stuttgart.de>
2024-12-04 17:00:10 +0000

    Managed Database connection

---
+++ Migrations/20241204161535_InitializeDatabase.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Mindful_Ocean_Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitializeDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "LandingPageContents",
+                columns: table => new
+                {
+                    Content_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(type: "longtext", nullable: false),
+                    Content = table.Column<string>(type: "longtext", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LandingPageContents", x => x.Content_id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "OceanDepths",
+                columns: table => new
+                {
+                    Longitude = table.Column<double>(type: "double", nullable: false),
+                    Latitude = table.Column<double>(type: "double", nullable: false),
+                    Depth = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OceanDepths", x => new { x.Longitude, x.Latitude });
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "PlasticWaste",
+                columns: table => new
+                {
+                    Entity = table.Column<string>(type: "varchar(255)", nullable: false),
+                    Code = table.Column<string>(type: "longtext", nullable: true),
+                    Year = table.Column<int>(type: "int", nullable: true),
+                    Plastic_Waste = table.Column<double>(type: "double", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlasticWaste", x => x.Entity);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LandingPageContents");
+
+            migrationBuilder.DropTable(
+                name: "OceanDepths");
+
+            migrationBuilder.DropTable(
+                name: "PlasticWaste");
+        }
+    }
+}
